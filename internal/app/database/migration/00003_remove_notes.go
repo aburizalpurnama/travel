e0dcfe19f29af3387383eb37cd75509558a29805
rizal purnama <purnamar9@gmail.com>
2025-11-02 01:06:34 +0700

    chore: remove notes on core.products

---
+++ internal/app/database/migration/00003_remove_notes.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upRemoveNotes, downRemoveNotes)
+}
+
+func upRemoveNotes(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	_, err := tx.ExecContext(ctx, "ALTER TABLE core.products DROP IF EXISTS notes;")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func downRemoveNotes(ctx context.Context, tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	_, err := tx.ExecContext(ctx, "ALTER TABLE core.products ADD IF NOT EXISTS notes VARCHAR(100) NULL;")
+	if err != nil {
+		return err
+	}
+	return nil
+}
